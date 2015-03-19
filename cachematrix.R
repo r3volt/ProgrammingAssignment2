beaf56d960e51ec0e0f1158b66d8f3603bd98f8f
Chris Miller <chris@swatman-miller.com>
2015-03-18 22:16:23 -0400

    foverview comments and boilerplate

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cache Matrix provides read-through cache optimized matrix inversion 
+## functionality. 
 
-## Write a short comment describing this function
+## High-level requirements overview:
+##  Computing the inverse of a square matrix can be done with the solve 
+##  function in R. For example, if X is a square invertible matrix, then 
+##  solve(X) returns its inverse.
+
+## Functions: 
+##  makeCacheMatrix: This function creates a special "matrix" object that can 
+##      cache its inverse.
+##  cacheSolve: This function computes the inverse of the special "matrix" 
+##      returned by makeCacheMatrix above. If the inverse has already been 
+##      calculated (and the matrix has not changed), then the cachesolve should
+##      retrieve the inverse from the cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+#     m <- NULL
+#     set <- function(y) {
+#         x <<- y
+#         m <<- NULL
+#     }
+#     get <- function() x
+#     setmean <- function(mean) m <<- mean
+#     getmean <- function() m
+#     list(set = set, get = get,
+#          setmean = setmean,
+#          getmean = getmean)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+#     m <- x$getmean()
+#     if(!is.null(m)) {
+#         message("getting cached data")
+#         return(m)
+#     }
+#     data <- x$get()
+#     m <- mean(data, ...)
+#     x$setmean(m)
+#     m    
 }
