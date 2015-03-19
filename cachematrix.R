f7f349e2376d5b6931e90fe61d52b12ae68ab512
Chris Miller <chris@swatman-miller.com>
2015-03-18 23:00:49 -0400

    implemented methods and tested per example specification

--- cachematrix.R
+++ cachematrix.R
@@ -14,32 +14,48 @@
 ##      calculated (and the matrix has not changed), then the cachesolve should
 ##      retrieve the inverse from the cache.
 
+## Use Conditions:
+##  Assumes we are using `solve()` method which throws an error in the following
+##  situations:
+##      1: non-square matrix (i.e. not 2x2 or 10x10, etc.)
+##      2: matrix is singular
+
+## Tested Matrixes:
+##  matrix(1:4,2,2)
+##      Small enough matrix to test functionality works per example usage. 
+##      No performance improvements given the size of the test subject.
+##  matrix(rnorm(10000),100,100)
+##      Show, using system.time() clear signs of improvement using cached 
+##      object versus regenerating each time.
+
 makeCacheMatrix <- function(x = matrix()) {
-#     m <- NULL
-#     set <- function(y) {
-#         x <<- y
-#         m <<- NULL
-#     }
-#     get <- function() x
-#     setmean <- function(mean) m <<- mean
-#     getmean <- function() m
-#     list(set = set, get = get,
-#          setmean = setmean,
-#          getmean = getmean)
+    ms <- NULL
+    set <- function(y) {
+        x <<- y
+        ms <<- NULL
+    }
+    get <- function() x
+    setSolve <- function(ms) ms <<- ms
+    getSolve <- function() ms
+    list(set = set, get = get,
+         setSolve = setSolve,
+         getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+## cacheSolve method is used to return a cached value for a given
+## makeCacheMatrix or generate one if not-exists. 
+## The generated cached inverted matrix is then also available through
+## the makeCacheMatrix object.
+cacheSolve <- function(x, ...) {    
     ## Return a matrix that is the inverse of 'x'
-#     m <- x$getmean()
-#     if(!is.null(m)) {
-#         message("getting cached data")
-#         return(m)
-#     }
-#     data <- x$get()
-#     m <- mean(data, ...)
-#     x$setmean(m)
-#     m    
+    ms <- x$getSolve()
+    if(!is.null(ms)) {
+        message("getting cached data")
+        return(ms)
+    }
+    data <- x$get()
+    ms <- solve(data, ...)
+    x$setSolve(ms)
+    ms    
 }
